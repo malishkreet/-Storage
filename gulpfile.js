8983aac9e373aa16782f7b4d5d35c9fe4b8e1674
malishkreet <maks15968@mail.ru>
2020-05-06 16:07:00 +0300

    feat: add gulp-sass in gilpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,27 @@
-const gulp = require('gulp');
+const {src, dest, watch} = require('gulp');
 const browserSync = require('browser-sync').create();
-var minifycss = require('gulp-minify-css');
-
-
-gulp.task('hello', function(done){
-    console.log ('Привет мир!');
-    done();
-});
-
+const sass = require('gulp-sass')
 
 
 // Static server
-gulp.task('browser-sync', function() {
+ function bs() {
+     serveSass();
     browserSync.init({
         server: {
             baseDir: "./"
         }
     });
-    gulp.watch("./*.html").on('change', browserSync.reload);
-});
+    watch("./*.html").on('change', browserSync.reload);
+    watch("./sass/**/*.sass", serveSass);
+    watch("./js/*.js").on('change', browserSync.reload);
+};
 
-// CSS Минимизация 
-gulp.task('css', function() {
-    return gulp.src('./css/*.css')
-      .pipe(minifycss())
-      .pipe(gulp.dest('./css'));
-  });
+
+ function serveSass() {
+    return src("./scss/*.scss")
+        .pipe(sass())
+        .pipe(dest("./css"))
+        .pipe(browserSync.stream());
+};
+
+exports.serve = bs;
