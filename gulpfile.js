b2188cb87ef25b968536f47893aadf4552a04a21
malishkreet <maks15968@mail.ru>
2020-05-07 11:26:40 +0300

    feat: add Futura Book & Demi fonts

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const {src, dest, watch} = require('gulp');
 const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass')
+const autoprefixer = require('gulp-autoprefixer');
 
 
 // Static server
@@ -18,10 +19,15 @@ const sass = require('gulp-sass')
 
 
  function serveSass() {
-    return src("./sass/*.sass")
+    return src("./sass/**/*.sass")
         .pipe(sass())
+        .pipe(autoprefixer({
+            cascade: false
+        }))
         .pipe(dest("./css"))
         .pipe(browserSync.stream());
 };
+
+
 
 exports.serve = bs;
