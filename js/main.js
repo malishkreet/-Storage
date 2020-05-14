3bee905854859fca488dd5bda8a2708c49f28433
malishkreet <maks15968@mail.ru>
2020-05-14 11:50:59 +0300

    feat: JQuery, jQuery script for the up button

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
+/* document.addEventListener("DOMContentLoaded", function(event) { 
     const modal = document.querySelector('.modal');
     const modalBtn = document.querySelectorAll('[data-toggle=modal]');
     const closeBtn = document.querySelector('.modal__close');
@@ -9,4 +9,62 @@ document.addEventListener("DOMContentLoaded", function(event) {
         element.addEventListener('click', switchModal);
     });
     closeBtn.addEventListener('click', switchModal)
+    
+  });
+ */
+
+ $(document). ready(function() {
+     var modal = $('.modal'),
+        modalBtn = $('[data-toggle="modal"]'),
+        closeBtn = $('.modal__close');
+    
+    modalBtn.on('click', function () {
+        modal.toggleClass('modal--visible');
+    });
+    closeBtn.on ('click', function() {
+        modal.toggleClass('modal--visible');
+    });
+ });
+
+
+ (function(jq) {
+    jq.autoScroll = function(ops) {
+      ops = ops || {};
+      ops.styleClass = ops.styleClass || 'button-up';
+      var t = jq('<div class="'+ops.styleClass+'"></div>'),
+     d = jq(ops.target || document);
+     jq(ops.container || 'body').append(t);
+   
+    t.css({
+      opacity: 0,
+      position: 'absolute',
+      top: 0,
+      right: 0
+   }).on('click', function() {
+       jq('html,body').animate({
+          scrollTop: 0
+     }, ops.scrollDuration || 1000);
+   });
+   
+    d.scroll(function() {
+      var sv = d.scrollTop();
+      if (sv < 300) {
+        t.clearQueue().fadeOut(ops.hideDuration || 200);
+       return;
+    }
+   
+    t.css('display', '').clearQueue().animate({
+     top: sv,
+     opacity: 0.8
+     }, ops.showDuration || 500);
+    });
+    };
+  })(jQuery);
+   
+  $(document).ready(function(){
+   $.autoScroll({
+   scrollDuration: 600, 
+   showDuration: 600, 
+   hideDuration: 300
+   });
   });
